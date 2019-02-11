30cc28cece6f276aae51e84e86e7a446eac25fb7
Daniel Barclay <daniel@urbanintel.io>
2019-02-11 00:48:45 +0000

    Added winning logic to complete CLI version of Connect 4

--- connect-4.py
+++ connect-4.py
@@ -15,7 +15,7 @@ def create_board():
 
 
 def drop_piece(board, row, col, piece):
-    # 0 - empty, 1 - player 1 piece, 2 - player2 piece
+    # 0 - empty space, 1 - player 1 piece, 2 - player2 piece
     board[row][col] = piece
 
 
@@ -37,9 +37,37 @@ def change_board_orientation(board):
     print(np.flip(board, 0))
 
 
+def winning_move(board, piece):
+    # check row for 4 in a row: horizontal
+    for c in range(COL_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    # check row for 4 in a row: vertical
+    for c in range(COL_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
+    # check row for 4 in a row: negative diagonal
+    for c in range(COL_COUNT - 3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and \
+                    board[r - 3][c + 3] == piece:
+                return True
+
+    # check row for 4 in a row: positive diagonal
+    for c in range(COL_COUNT - 3):
+        for r in range(ROW_COUNT - 3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and \
+                    board[r+3][c+3] == piece:
+                return True
+
+
 # game status
 game_status = True
-turn = 89
+turn = 0
 
 # instantiations
 board = create_board()
@@ -53,6 +81,10 @@ while game_status:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, PLAYER_1_PIECE)
+            if winning_move(board, PLAYER_1_PIECE):
+                print('Player 1 wins!')
+                game_status = False
+                break
 
     if turn % 2 != 0:
         col = int(input('Player 2 - Make your move! (0-6): '))
@@ -60,6 +92,10 @@ while game_status:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, PLAYER_2_PIECE)
+            if winning_move(board, PLAYER_2_PIECE):
+                print('Player 2 wins!')
+                game_status = False
+                break
 
     turn += 1
     change_board_orientation(board)
