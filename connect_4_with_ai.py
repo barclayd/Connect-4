6e44cac72d26ce97688501774480850ee8e3bf8b
Daniel Barclay <barclaysd@me.com>
2019-04-20 00:52:46 +0100

    AI now evaluates best horizontal move it can make and places pieces based on scoring logic

--- connect_4_with_ai.py
+++ connect_4_with_ai.py
@@ -7,8 +7,10 @@ import random
 # setup of connect 4 matrix
 COL_COUNT = 7
 ROW_COUNT = 6
-PLAYER_1_PIECE = 1
-PLAYER_2_PIECE = 2
+PLAYER_PIECE = 1
+AI_PIECE = 2
+EMPTY_PIECE = 0
+WINDOW_LENGTH = 4
 
 # players
 PLAYER = 0
@@ -89,9 +91,9 @@ def draw_board(board):
             pygame.draw.circle(screen, BLACK, (int(c * SQUARE_SIZE + SQUARE_SIZE / 2), int(r * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE / 2)), RADIUS)
     for c in range(COL_COUNT):
         for r in range(ROW_COUNT):
-            if board[r][c] == 1:
+            if board[r][c] == PLAYER_PIECE:
                 draw_circle(c, r, RED)
-            if board[r][c] == 2:
+            if board[r][c] == AI_PIECE:
                 draw_circle(c, r, YELLOW)
     pygame.display.update()
 
@@ -109,6 +111,48 @@ def winning_text():
         winning_player = 'Player 2'
         label = win_text.render("{} wins!".format(winning_player), 1, RED)
     screen.blit(label, (40, 10))
+
+
+def score_position(board, piece):
+    score = 0
+    # check horizontal score
+    for r in range(ROW_COUNT):
+        row_array = [int(i) for i in list(board[r, :])]
+        for c in range(COL_COUNT-3):
+            # breaking matrix up into slices of 4
+            window = row_array[c:c+WINDOW_LENGTH]
+            if window.count(piece) == 4:
+                score += 100
+            elif window.count(piece) == 3 and window.count(EMPTY_PIECE) == 1:
+                score += 10
+    return score
+
+
+def get_valid_locations(board):
+    valid_locations = []
+    for column in range(COL_COUNT):
+        if is_valid_location(board, column):
+            valid_locations.append(column)
+    return valid_locations
+
+
+def choose_optimum_move(board, piece):
+    # returns the column of the best column in which to drop the next piece
+    best_score = 0
+    valid_locations = get_valid_locations(board)
+    best_col = random.choice(valid_locations)
+    # simulate dropping in a piece to calculate highest possible scores
+    for column in valid_locations:
+        row = get_next_open_row(board, column)
+        # copy existing board to create a temporary board without modifying existing board
+        temp_board = board.copy()
+        drop_piece(temp_board, row, column, piece)
+        score = score_position(temp_board, piece)
+        if score > best_score:
+            best_score = score
+            best_col = column
+
+    return best_col
 
 
 # game status
@@ -144,10 +188,8 @@ while not game_over:
         if event.type == pygame.MOUSEMOTION:
             pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARE_SIZE))
             pos_x = event.pos[0]
-            if turn % 2 == PLAYER:
+            if turn == PLAYER:
                 pygame.draw.circle(screen, RED, (pos_x, int(SQUARE_SIZE/2)), RADIUS)
-            else:
-                pygame.draw.circle(screen, YELLOW, (pos_x, int(SQUARE_SIZE/2)), RADIUS)
         pygame.display.update()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -158,8 +200,8 @@ while not game_over:
 
                 if is_valid_location(board, col):
                     row = get_next_open_row(board, col)
-                    drop_piece(board, row, col, PLAYER_1_PIECE)
-                    if winning_move(board, PLAYER_1_PIECE):
+                    drop_piece(board, row, col, PLAYER_PIECE)
+                    if winning_move(board, PLAYER_PIECE):
                         label = win_text.render("Player 1 wins!", 1, YELLOW)
                         screen.blit(label, (40, 10))
                         game_over = True
@@ -169,12 +211,13 @@ while not game_over:
     # AI move
     if turn == AI and not game_over:
 
-        col = random.randint(0, COL_COUNT-1)
+        # col = random.randint(0, COL_COUNT-1)
+        col = choose_optimum_move(board, AI_PIECE)
 
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
-            drop_piece(board, row, col, PLAYER_2_PIECE)
-            if winning_move(board, PLAYER_2_PIECE):
+            drop_piece(board, row, col, AI_PIECE)
+            if winning_move(board, AI_PIECE):
                 label = win_text.render("Player 1 wins!", 1, YELLOW)
                 screen.blit(label, (40, 10))
                 game_over = True
